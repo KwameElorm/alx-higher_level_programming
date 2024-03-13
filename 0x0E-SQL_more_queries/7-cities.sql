6ce9a75cf6f58f56f639889f37243be6bdf55aed
KwameElorm <elormh@ymail.com>
2024-03-13 13:41:18 -0700

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
---creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
--- creates db
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- creates a db
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
 USE hbtn_0d_usa;
--- creates tables and values
+-- creates a table
 CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
