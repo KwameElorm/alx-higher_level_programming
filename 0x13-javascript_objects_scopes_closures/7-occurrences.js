6623f8634d372e495eec7d7ef9246be99094601c
KwameElorm <elormh@ymail.com>
2024-04-23 08:30:38 -0700

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,15 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-  return function (dec) {
-    return dec.toString(base);
-  };
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  list.map(
+    (x) => {
+      if (x === searchElement) {
+        count += 1;
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  );
+  return count;
 };
